1deaa1c3b16bebe701d9f245609e3675901c204b
Victor Leandro <victorleandro430@gmail.com>
2024-01-25 22:41:05 -0300

    Feature - adicionando rotas para as páginas

---
+++ frontend/src/pages/ClientRegister.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h4>Asadad</h4>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ClientRegister',
+  components: {
+    
+  }
+}
+</script>
---
+++ frontend/src/pages/OrderView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h4>Order</h4>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'OrderView',
+  
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
